b56f60e060368a3bdea7ceaeae91a9ae96b1027a
unknown <oyebanjoabiola@gmail.com>
2026-04-01 12:56:09 +0100

    update: real logos, corrected About page, LSETF/ELP content

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__, static_folder='.')
 
 @app.route('/')
 def index():
     return send_from_directory('.', 'index.html')
+
+@app.route('/public/<path:filename>')
+def public(filename):
+    return send_from_directory('public', filename)
 
 if __name__ == '__main__':
     app.run()
